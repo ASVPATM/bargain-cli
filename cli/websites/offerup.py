09e71e2cfd56ebac0c74d9f423001bd205469715
ASVPATM <159084542+ASVPATM@users.noreply.github.com>
2024-06-11 22:42:19 -0700

    Add files via upload

---
+++ cli/websites/offerup.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.append('/home/asvplen/cli/city_wordlist')
+sys.path.append('/home/asvplen/cli/websites')
+import craigslist, offerup, facebook_marketplace, ebay, temu, ali_express, ultimate
+import cities
+
+
+#OFFERUP WILL GET DIFFERENT HTML TAGS DEPENDING ON IF LOCAL OR SHIPPING WAS CHOSEN
+
+#MAIN CODE HERE
+def main():
+    pass
+
+
+def getEverything(city_full_name, transport_method, query):
+        if transport_method == 2:
+            return None
+        cities.shorten_city_name(city_full_name)
+
+
+
+
+if __name__ == "__main__":
+    main()
